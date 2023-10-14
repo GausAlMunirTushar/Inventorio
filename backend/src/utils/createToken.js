aab79a87260c07c00bce397e69833a3a89634d49
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-10-15 04:59:53 +0600

    create token completed

--- backend/src/utils/createToken.js
+++ backend/src/utils/createToken.js
@@ -1,11 +1,25 @@
 const jwt = require('jsonwebtoken');
 
-const createToken = async (data) => {
-    let payload = {
-        exp: Math.floor(Date.now() / 100 + (24*60*60)),
-        data: data,
+const jwtSecret = process.env.JWT_SECRET;
+
+/**
+ * Create a JSON Web Token (JWT) with the provided data.
+ * @param {Object} data - The data to be included in the token payload.
+ * @returns {string} - The JWT token.
+ */
+
+function createToken(data) {
+    try {
+        const expiresIn = 24 * 60 * 60; // 24 hours in seconds
+        const payload = {
+            exp: Math.floor(Date.now() / 1000) + expiresIn,
+            data: data,
+        };
+        return jwt.sign(payload, jwtSecret);
+    } catch (error) {
+        console.error('Error creating JWT:', error);
+        throw error;
     }
-    return await jwt.sign(payload, 'SecretKey123456789');
 }
 
 module.exports = createToken;
