c3db96c09de1611b1c18d3ee1de72d2b7b717641
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-10-19 22:48:55 +0600

    frontend project intilize

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <h1> Web Application is currently under construction</h1>
+      <p>Thank you for visiting our website. We are currently in the process of making some important updates and improvements to provide you with a better online experience. During this time, you may encounter some temporary inconveniences, but we promise to be back online as soon as possible.</p>
+    </>
+  )
+}
+
+export default App
